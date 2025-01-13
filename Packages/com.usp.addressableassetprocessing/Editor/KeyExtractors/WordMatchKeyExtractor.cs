45bdcd6c0513af0f79820cf80511e4f711f43bc2
Joel Barba <joel.barba@unity3d.com>
2025-01-12 22:03:19 -0600

    Generalize the WordMatchKeyExytractor further.

--- Packages/com.usp.addressableassetprocessing/Editor/KeyExtractors/WordMatchKeyExtractor.cs
+++ Packages/com.usp.addressableassetprocessing/Editor/KeyExtractors/WordMatchKeyExtractor.cs
@@ -6,6 +6,13 @@ namespace USP.AddressablesAssetProcessing
 {
     public class WordMatchKeyExtractor : MatchKeyExtractor
     {
+        #region Static Methods
+        private static bool Check(Match match)
+        {
+            return match.Success && !string.IsNullOrEmpty(match.Value);
+        }
+        #endregion
+
         #region Properties
         public Dictionary<string, List<string>> Transform = new Dictionary<string, List<string>>();
         #endregion
@@ -19,29 +26,48 @@ namespace USP.AddressablesAssetProcessing
 
         private void ExtractWords(Match match, string original, HashSet<string> ignored, HashSet<string> result)
         {
-            while (match.Success && !string.IsNullOrEmpty(match.Value))
+            // If there is initial match, then:
+            if (!Check(match))
             {
-                if (ignored.Contains(match.Value))
-                {
-                    match = match.NextMatch();
+                // Add the value as-is.
+                KeyExtractor.Add(original, ignored, result);
 
-                    continue;
+                // Do nothing else.
+                return;
+            }
+
+            // Otherwise, there is one match.
+
+            do
+            {
+                // If the word is not contained in the set of ignored words, then the word should not be ignored.
+                // If the word should not be ignored, then:
+                if (!ignored.Contains(match.Value))
+                {
+                    // Attempt to find words that are associated with that word.
+                    bool found = Transform.TryGetValue(match.Value, out List<string> words);
+                    
+                    // If there were no associated words found, then: 
+                    if (!found)
+                    {
+                        // Attempt to identify any camel-case words and spit them. 
+                        words = KeyExtractor.SplitByCamelCase(match.Value);
+                    }
+
+                    // There is at least one word in the list of words.
+
+                    // For every word in the list of words, perform the following.
+                    foreach (var word in words)
+                    {
+                        // Add the word to the list of extracted keys.
+                        KeyExtractor.Add(word, ignored, result);
+                    }
                 }
 
-                bool found = Transform.TryGetValue(match.Value, out List<string> words);
-
-                if (!found)
-                {
-                    words = KeyExtractor.SplitByCamelCase(match.Value);
-                }
-
-                foreach (var word in words)
-                {
-                    KeyExtractor.Add(word, ignored, result);
-                }
-
+                // Move onto the next item in the matches.
                 match = match.NextMatch();
             }
+            while (Check(match));
         }
         #endregion
     }
