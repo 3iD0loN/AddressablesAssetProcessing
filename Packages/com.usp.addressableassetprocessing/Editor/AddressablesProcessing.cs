3be30558d31730c75d1fe912c1a533d8a30c41ac
Joel Barba <joel.barba@unity3d.com>
2025-02-13 09:13:52 -0600

    Updated AddressavleAssetProcessing code to make use of the new AssetData interface, which simplifies it

--- Packages/com.usp.addressableassetprocessing/Editor/AddressablesProcessing.cs
+++ Packages/com.usp.addressableassetprocessing/Editor/AddressablesProcessing.cs
@@ -5,6 +5,7 @@ using UnityEditor.AddressableAssets;
 
 namespace USP.AddressablesAssetProcessing
 {
+    using UnityEditor.VersionControl;
     using USP.MetaAddressables;
 
     public static class AddressablesProcessing
@@ -38,35 +39,46 @@ namespace USP.AddressablesAssetProcessing
             IGroupSelector groupSelector,
             IKeyExtractor<string, HashSet<string>> labelExtractor)
         {
+            // Apply the asset file path to the group selector to set the appropriate group template.
             groupSelector.Apply(assetFilePath);
 
+            // Get the user data associated with the asset file path.
             MetaAddressables.UserData userData = MetaAddressables.Read(assetFilePath);
 
+            // If there was no valid user data associated with the asset file path, then:
             if (userData == null)
             {
+                // Do nothing else.
                 return;
             }
+
+            // Otherwise, there is valid user data associated with the asset file path.
 
             // Extract labels from the folder path.
             var extractedLabels = new HashSet<string>();
             labelExtractor.Extract(assetFilePath, extractedLabels);
 
-            // Add the extracted labels to the labels associated with the asset.
-            extractedLabels.UnionWith(userData.Asset.Labels);
-            proccessedlabels.UnionWith(extractedLabels);
-            userData.Asset.Labels = extractedLabels.ToList();
+            // Union the labels that were extracted with the current ones associated with the asset.
+            userData.Asset.Labels.UnionWith(extractedLabels);
 
+            // Take the current address of the asset and simplify it.
             userData.Asset.Address = MetaAddressables.AssetData.SimplifyAddress(userData.Asset.Address);
 
+            // For tracing: Add the user data andlabelsto the collections of items that were processed.
             processedUserDatas.Add(userData);
+            proccessedlabels.UnionWith(userData.Asset.Labels);
 
             var settings = AddressableAssetSettingsDefaultObject.Settings;
-            var labels = settings.GetLabels();
-            var labelsSet = new HashSet<string>(labels);
-            labelsSet.UnionWith(extractedLabels);
-            foreach (var label in labelsSet)
+
+            // If there are valid settings, then:
+            if (settings != null)
             {
-                settings.AddLabel(label);
+                // For every label extracted from the asset path, perform the following:
+                foreach (var label in extractedLabels)
+                {
+                    // Attempt to add the label to the global list.
+                    settings.AddLabel(label);
+                }
             }
 
             // Generate Addressables groups from the Meta file.
