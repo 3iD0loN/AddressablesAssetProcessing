675c6722ca472910cf9c16e2ef970ed69b0bf789
Joel Barba <joel.barba@unity3d.com>
2025-02-16 10:25:13 -0600

    Deduplicator code now can select the appropriate group template from a set of groups that the dependent assets in the build are defined by.

--- Packages/com.usp.addressableassetprocessing/Editor/AddressablesDeduplicator.cs
+++ Packages/com.usp.addressableassetprocessing/Editor/AddressablesDeduplicator.cs
@@ -1,24 +1,45 @@
+using System.Collections.Generic;
 using System.Linq;
 
-using UnityEngine;
-
 using UnityEditor;
-using UnityEditor.AddressableAssets.Build.BuildPipelineTasks;
-using UnityEditor.AddressableAssets.Build.DataBuilders;
-using UnityEditor.Build.Pipeline;
-
-using USP.AddressablesBuildGraph;
-using UnityEditor.VersionControl;
 using UnityEditor.AddressableAssets;
-using USP.MetaAddressables;
+using UnityEditor.AddressableAssets.Settings;
 using USP.AddressablesAssetProcessing;
+using USP.AddressablesBuildGraph;
+using USP.MetaAddressables;
 
-
+//*/
 public class AddressablesDeduplicator
 {
     #region Static Methods
     [MenuItem("Tools/Addressables Deduplication")]
     private static void Run()
+    {
+        var x = new Dictionary<int, List<AddressableAssetGroupTemplate>>();
+
+        var settings = AddressableAssetSettingsDefaultObject.Settings;
+        foreach (AddressableAssetGroupTemplate groupTemplate in settings.GroupTemplateObjects)
+        {
+            if (groupTemplate == null)
+            {
+                return;
+            }
+
+            var y = new MetaAddressables.GroupData(groupTemplate);
+            bool found = x.TryGetValue(y.GetHashCode(),
+                out List<AddressableAssetGroupTemplate> groupTemplateList);
+
+            if (!found)
+            {
+                groupTemplateList = new List<AddressableAssetGroupTemplate>();
+                x.Add(y.GetHashCode(), groupTemplateList);
+            }
+
+            groupTemplateList.Add(groupTemplate);
+        }
+    }
+
+    public static void ProcessAssets(IGroupSelector<HashSet<MetaAddressables.GroupData>> groupSelector)
     {
         var settings = AddressableAssetSettingsDefaultObject.Settings;
         var buildInfo = AddressablesBuildInfo.Create(settings);
@@ -29,17 +50,54 @@ public class AddressablesDeduplicator
         }
 
         var duplicatedImplicitRoots = from asset in buildInfo.Assets
-                                                 where asset.IsDuplicate && asset.IsImplicitRoot
-                                                 select asset;
+                                      where asset.IsValid && asset.IsDuplicate && asset.IsImplicitRoot
+                                      select asset;
 
         foreach (AssetInfo duplicatedImplicitRoot in duplicatedImplicitRoots)
         {
-            string assetFilePath = duplicatedImplicitRoot.FilePath;
+            ProcessAsset(settings, groupSelector, duplicatedImplicitRoot);
+        }
+    }
 
-            // Apply the asset file path to the group selector to set the appropriate group template.
-            //groupSelector.Apply(assetFilePath);
+    private static void ProcessAsset(AddressableAssetSettings settings,
+        IGroupSelector<HashSet<MetaAddressables.GroupData>> groupSelector,
+        AssetInfo duplicatedImplicitRoot)
+    {
+        string assetFilePath = duplicatedImplicitRoot.FilePath;
 
-            MetaAddressables.UserData userData = MetaAddressables.Read(assetFilePath);
+        var groups = new HashSet<MetaAddressables.GroupData>(MetaAddressables.GroupData.Comparer.ByNameAndHash);
+        PopulateGroups(duplicatedImplicitRoot.DependentAssets, groups);
+
+        // Apply the asset file path to the group selector to set the appropriate group template.
+        groupSelector.Apply(groups);
+
+        //MetaAddressables.UserData userData = MetaAddressables.Read(assetFilePath);
+    }
+
+    private static void PopulateGroups(HashSet<AssetInfo> assetInfoSet, HashSet<MetaAddressables.GroupData> groupSet)
+    {
+        foreach (AssetInfo dependentAsset in assetInfoSet)
+        {
+            PopulateGroups(dependentAsset, groupSet);
+        }
+    }
+
+    private static void PopulateGroups(AssetInfo assetInfo, HashSet<MetaAddressables.GroupData> groupSet)
+    {
+        foreach (AssetBundleInfo bundleInfo in assetInfo.Bundles)
+        {
+            var groupGuid = bundleInfo.Group.Guid;
+
+            bool found = AddressablesGroupLookup.GroupsByGuids.TryGetValue(groupGuid, out AddressableAssetGroup group);
+
+            if (!found)
+            {
+                break;
+            }
+
+            var groupData = new MetaAddressables.GroupData(group);
+
+            groupSet.Add(groupData);
         }
     }
     #endregion
