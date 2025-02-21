754426daa45d6ad65fe3038863587101ed0bc2f2
Joel Barba <joel.barba@unity3d.com>
2025-02-20 20:07:14 -0600

    Update AddressablesProcessing.cs

--- Packages/com.usp.addressableassetprocessing/Editor/AddressablesProcessing.cs
+++ Packages/com.usp.addressableassetprocessing/Editor/AddressablesProcessing.cs
@@ -4,24 +4,16 @@ using UnityEditor.AddressableAssets;
 
 namespace USP.AddressablesAssetProcessing
 {
+    using UnityEditor.AddressableAssets.Settings;
     using USP.MetaAddressables;
 
     public static class AddressablesProcessing
     {
-        #region Static Fields
-        public static List<MetaAddressables.UserData> processedUserDatas = new List<MetaAddressables.UserData>();
-
-        public static HashSet<string> proccessedlabels = new HashSet<string>();
-        #endregion
-
         #region Static Methods
         public static void ProcessAssets(IEnumerable<string> assetFilePaths,
             IGroupSelector<string> groupSelector,
             IKeyExtractor<string, HashSet<string>> labelExtractor)
         {
-            processedUserDatas.Clear();
-            proccessedlabels.Clear();
-
             foreach (string assetFilePath in assetFilePaths)
             {
                 ProcessAsset(assetFilePath, groupSelector, labelExtractor);
@@ -40,6 +32,18 @@ namespace USP.AddressablesAssetProcessing
             // Apply the asset file path to the group selector to set the appropriate group template.
             groupSelector.Apply(assetFilePath);
 
+            // Extract labels from the folder path.
+            var extractedLabels = new HashSet<string>();
+            labelExtractor.Extract(assetFilePath, extractedLabels);
+
+            SetAddressableAsset(assetFilePath, extractedLabels);
+
+            var settings = AddressableAssetSettingsDefaultObject.Settings;
+            SetLabels(settings, extractedLabels);
+        }
+
+        private static void SetAddressableAsset(string assetFilePath, HashSet<string> labels)
+        {
             // Get the user data associated with the asset file path.
             MetaAddressables.UserData userData = MetaAddressables.Read(assetFilePath);
 
@@ -50,34 +54,11 @@ namespace USP.AddressablesAssetProcessing
                 return;
             }
 
-            // Otherwise, there is valid user data associated with the asset file path.
-
-            // Extract labels from the folder path.
-            var extractedLabels = new HashSet<string>();
-            labelExtractor.Extract(assetFilePath, extractedLabels);
-
             // Union the labels that were extracted with the current ones associated with the asset.
-            userData.Asset.Labels.UnionWith(extractedLabels);
+            userData.Asset.Labels.UnionWith(labels);
 
             // Take the current address of the asset and simplify it.
             userData.Asset.Address = MetaAddressables.AssetData.SimplifyAddress(userData.Asset.Address);
-
-            // For tracing: Add the user data andlabelsto the collections of items that were processed.
-            processedUserDatas.Add(userData);
-            proccessedlabels.UnionWith(userData.Asset.Labels);
-
-            var settings = AddressableAssetSettingsDefaultObject.Settings;
-
-            // If there are valid settings, then:
-            if (settings != null)
-            {
-                // For every label extracted from the asset path, perform the following:
-                foreach (var label in extractedLabels)
-                {
-                    // Attempt to add the label to the global list.
-                    settings.AddLabel(label);
-                }
-            }
 
             // Generate Addressables groups from the Meta file.
             // This is done before saving MetaAddressables to file in case we find groups that already match.
@@ -85,6 +66,22 @@ namespace USP.AddressablesAssetProcessing
 
             // Save to MetaAddressables changes.
             MetaAddressables.Write(assetFilePath, userData);
+        }
+
+        private static void SetLabels(AddressableAssetSettings settings, HashSet<string> labels)
+        {
+            // If there are valid settings, then:
+            if (settings == null)
+            {
+                return;
+            }
+
+            // For every label extracted from the asset path, perform the following:
+            foreach (var label in labels)
+            {
+                // Attempt to add the label to the global list.
+                settings.AddLabel(label);
+            }
         }
         #endregion
     }
