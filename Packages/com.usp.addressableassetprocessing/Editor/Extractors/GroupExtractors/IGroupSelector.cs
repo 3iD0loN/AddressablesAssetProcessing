f8e0744c511178bc9416b387dac33d64c2f7dac6
Joel Barba <joel.barba@unity3d.com>
2025-02-27 00:24:01 -0600

    Renaming folder GroupSelectors to GroupExtractors.

--- Packages/com.usp.addressableassetprocessing/Editor/Extractors/GroupExtractors/IGroupSelector.cs
+++ Packages/com.usp.addressableassetprocessing/Editor/Extractors/GroupExtractors/IGroupSelector.cs
@@ -2,7 +2,7 @@ using UnityEditor.AddressableAssets.Settings;
 
 namespace USP.AddressablesAssetProcessing
 {
-    public interface IGroupSelector<T> : IKeyExtractor<T, AddressableAssetGroupTemplate>
+    public interface IGroupSelector<T> : IExtractor<T, AddressableAssetGroupTemplate>
     {
     }
 }
