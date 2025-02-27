6cd7f2d2f4823ea5d3650c83f000b8b896b33edf
Joel Barba <joel.barba@unity3d.com>
2025-02-27 00:06:04 -0600

    Adding MetaAddressablesProcessing to a namespace

--- Packages/com.usp.addressableassetprocessing/Editor/MetaAddressablesProcessing.cs
+++ Packages/com.usp.addressableassetprocessing/Editor/MetaAddressablesProcessing.cs
@@ -1,49 +1,56 @@
-using UnityEngine;
+using System.Collections.Generic;
 
-public static class MetaAddressablesProcessing
+using UnityEditor.AddressableAssets.Settings;
+
+namespace USP.AddressablesAssetProcessing
 {
-    public static void SetAddressableAsset(string assetFilePath, AddressableAssetGroupTemplate group, string address, HashSet<string> labels)
+    using USP.MetaAddressables;
+
+    public static class MetaAddressablesProcessing
     {
-        // MetaAddressables data cration will default to using this group if there is no metadata associated. 
-        MetaAddressables.factory.ActiveGroupTemplate = group;
-
-        // Get the user data associated with the asset file path.
-        MetaAddressables.UserData userData = MetaAddressables.Read(assetFilePath);
-
-        // If there was no valid user data associated with the asset file path, then:
-        if (userData == null)
+        public static void SetAddressableAsset(string assetFilePath, AddressableAssetGroupTemplate group, string address, HashSet<string> labels)
         {
-            // Do nothing else.
-            return;
+            // MetaAddressables data cration will default to using this group if there is no metadata associated. 
+            MetaAddressables.factory.ActiveGroupTemplate = group;
+
+            // Get the user data associated with the asset file path.
+            MetaAddressables.UserData userData = MetaAddressables.Read(assetFilePath);
+
+            // If there was no valid user data associated with the asset file path, then:
+            if (userData == null)
+            {
+                // Do nothing else.
+                return;
+            }
+
+            // Union the labels that were extracted with the current ones associated with the asset.
+            userData.Asset.Labels.UnionWith(labels);
+
+            // Take the current address of the asset and simplify it.
+            userData.Asset.Address = address;
+
+            // Generate Addressables groups from the Meta file.
+            // This is done before saving MetaAddressables to file in case we find groups that already match.
+            MetaAddressables.Generate(userData);
+
+            // Save to MetaAddressables changes.
+            MetaAddressables.Write(assetFilePath, userData);
         }
 
-        // Union the labels that were extracted with the current ones associated with the asset.
-        userData.Asset.Labels.UnionWith(labels);
-
-        // Take the current address of the asset and simplify it.
-        userData.Asset.Address = address;
-
-        // Generate Addressables groups from the Meta file.
-        // This is done before saving MetaAddressables to file in case we find groups that already match.
-        MetaAddressables.Generate(userData);
-
-        // Save to MetaAddressables changes.
-        MetaAddressables.Write(assetFilePath, userData);
-    }
-
-    public static void SetGlobalLabels(AddressableAssetSettings settings, HashSet<string> labels)
-    {
-        // If there are valid settings, then:
-        if (settings == null)
+        public static void SetGlobalLabels(AddressableAssetSettings settings, HashSet<string> labels)
         {
-            return;
-        }
+            // If there are valid settings, then:
+            if (settings == null)
+            {
+                return;
+            }
 
-        // For every label extracted from the asset path, perform the following:
-        foreach (var label in labels)
-        {
-            // Attempt to add the label to the global list.
-            settings.AddLabel(label);
+            // For every label extracted from the asset path, perform the following:
+            foreach (var label in labels)
+            {
+                // Attempt to add the label to the global list.
+                settings.AddLabel(label);
+            }
         }
     }
 }
